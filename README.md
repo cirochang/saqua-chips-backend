e05493d62206262be8edbcb3e413376f40217b21
Ciro Chang <cirochang@live.com>
2017-11-03 00:52:41 -0200

    fixing backend

--- README.md
+++
@@ -1,2 +0,0 @@
-# saqua-chips-backend
-A backend system for a specific store
--- server.js
+++ server.js
@@ -1,15 +1,43 @@
-// server.js
-var express = require('express');
-var app = express();
-var port = 8080;
+// Grabs our environment variables from the .env file
+require('dotenv').config();
 
-//route our app
-var routes = require('./app/routes');
-app.use('/', routes);
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    cors = require('cors'),
+    app = express();
 
-app.use(express.static(__dirname + '/public'));
+// Environment configuration
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+var port = process.env.PORT || 3000;
 
-// start the server
-app.listen(port, function() {
-  console.log('app started');
+// Create a server side router
+var router = express.Router();
+
+// Configure express to handle HTTP requests
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(methodOverride());
+
+// Define the route endpoints for our application
+
+// Create a new charge
+router.post('/charge', function(req, res){
+  res.sendStatus(200); // equivalent to res.status(200).send('OK')
+});
+
+// Route to get the data for a charge filtered by the charge's id
+router.get('/charge/:id', function(req, res){
+  res.sendStatus(200); // equivalent to res.status(200).send('OK')
+});
+
+// Register the router
+app.use('/', router);
+
+// Start the server
+app.listen(port, function(){
+  console.log('Server listening on port ' + port)
 });
